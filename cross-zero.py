a8ac255a3bba8c05a75bd57dd8a20d6e7199785d
justbegginer <46086383+justbegginer@users.noreply.github.com>
2020-02-27 01:22:13 +0300

    make coordinate system

--- cross-zero.py
+++ cross-zero.py
@@ -1,14 +1,23 @@
 field = []
-for i in range(5):
+field.append([])
+field[0].append("  ")
+for i in range(3):
+    field[0].append(str(i))
+    field[0].append(" ")
+for i in range(1, 6):
     field.append([])
-    for ii in  range(5):
-        if i%2==1:
+    if i % 2 == 1 :
+        field[i].append(chr(ord("a") +(i // 2))+" ")
+    else:
+        field[i].append("  ")
+
+    for ii in range(5):
+        if i % 2 == 0:
             field[i].append("-")
         elif ii % 2 == 0:
             field[i].append(" ")
         else:
             field[i].append("|")
-
 
 
 def game():
@@ -26,13 +35,20 @@ def game():
 def is_full_field():
     pass
 
+
 def draw_field():
-    pass
+    for i in field:
+        print("".join(i))
+
 
 def move(who):
-    pass
+    def can_you_make_a_move(point):
+        return field[point[0]][point[1]] == " "
+    point = input("Введите точку")
+    return
+
 
 def is_win():
     pass
 
-game()
+draw_field()
