cd46f5dda5153239003f3343cddb4559c6cf34a2
justbegginer <46086383+justbegginer@users.noreply.github.com>
2020-02-27 00:58:05 +0300

    make the template

--- cross-zero.py
+++ cross-zero.py
@@ -1,1 +1,27 @@
+
+
 def game():
+    while True:
+        draw_field()
+        if is_win(move("0")):
+            print("zeros wins")
+            break
+        draw_field()
+        if is_win(move("*")):
+            print("crosses wins")
+            break
+
+
+def is_full_field():
+    pass
+
+def draw_field():
+    pass
+
+def move(who):
+    pass
+
+def is_win():
+    pass
+
+game()
